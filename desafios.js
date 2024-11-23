68dc3241af960a6ce0b185e847bcd1758ed05383
snovoa669 <ing.softwarenovoa2023@gmail.com>
2024-11-22 20:58:27 -0500

    clase funciones 22/11/2024

--- desafios.js
+++ desafios.js
@@ -26,7 +26,7 @@ Desafio3("Sergio", 34);*/
 la función debe imprimir en consola: Su nombre es ____ y usted tiene ____ años,
 si no se recibe la edad imprimir: Su nombre es ____ y usted tiene 100 años.*/
 
-function desafio4(nombre, apellido, edad = 100) {
+/*function desafio4(nombre, apellido, edad = 100) {
   console.log(
     "Su nombre es " +
       nombre +
@@ -39,3 +39,62 @@ function desafio4(nombre, apellido, edad = 100) {
 }
 desafio4("Sergio", "Novoa", 34);
 desafio4("Sergio", "Novoa");
+/*5. Crear una función que reciba dos párametros, un nombre y un apellido y retorne
+el nombre completo, primero el apellido y luego el nombre.*/
+/*
+function desafio5(nombre, apellido) {
+  return apellido + " " + nombre;
+}
+let nombrecompleto = desafio5("Sergio", "Novoa");
+let nombrecompanero = desafio5("Alvaro", "Bahos");
+console.log(nombrecompleto, nombrecompanero); */
+
+/*6. Crear una función que reciba cuatro párametros, un nombre, una prenda, un
+color y una cantidad y retorne: ____ tiene __ ____ de color ____.
+Ej entrada: pepita, camisas, azul, 2
+Ej salida: Pepita tiene 2 camisas de color azul.*/
+/*
+function desafio6(nombre, color, cantidad, prenda_Vestir) {
+  return `${nombre} tiene ${cantidad} ${prenda_Vestir} de color ${color}`;
+}
+let mensaje = desafio6("Pepita", "azul", 2, "camisas");
+let mensaje_propio = desafio6("Sergio", "rojo", 6, "busos");
+console.log(mensaje);
+console.log(mensaje_propio);*/
+/*
+function desafio7(_nombre = "desconcido") {
+  console.log("Hola," + _nombre);
+}
+desafio7("Sergio");
+desafio7();
+*/
+function suma(numero1, numero2) {
+  const resultado = numero1 + numero2;
+  console.log(
+    numero1 + " " + "+" + " " + " " + numero2 + " " + "=" + resultado
+  );
+}
+suma(8, 4);
+function resta(numero1, numero2) {
+  const resultado = numero1 - numero2;
+  console.log(numero1 + " " + "-" + " " + numero2 + " " + "=" + resultado);
+}
+resta(8, 4);
+
+function multiplicacion(numero1, numero2) {
+  const resultado = numero1 * numero2;
+  console.log(numero1 + " " + "*" + " " + numero2 + " " + "=" + resultado);
+}
+multiplicacion(8, 4);
+
+function division(numero1, numero2) {
+  const resultado = numero1 / numero2;
+  console.log(numero1 + " " + "/" + " " + numero2 + " " + "=" + resultado);
+}
+division(8, 4);
+
+function modulo(numero1, numero2) {
+  const resultado = numero1 % numero2;
+  console.log(numero1 + " " + "%" + " " + numero2 + " " + "=" + resultado);
+}
+modulo(8, 4);
